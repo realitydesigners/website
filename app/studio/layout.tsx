75963c1913265fd50e5d85c4df7a15d40473e8d7
RD <raymond.luke.spartz@gmail.com>
2023-11-26 23:32:51 -0800

    layout

---
+++ app/studio/layout.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from 'react';
+import { Footer } from '@/components/global/Footer';
+import Navbar from '@/components/global/Navbar';
+
+export default function IndexPage({ children }: { children: React.ReactNode }) {
+   return (
+      <main className="flex-grow bg-gray-200">
+         <Suspense
+            fallback={
+               <div className="flex justify-center items-center min-h-screen">
+                  <p>Loading...</p>
+               </div>
+            }
+         >
+            {children}
+         </Suspense>
+      </main>
+   );
+}
