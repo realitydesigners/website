d2a39ab399ae9a37e2a8ddc4405316637a934d15
RD <raymond.luke.spartz@gmail.com>
2023-11-26 23:21:04 -0800

    fix invariant issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,22 @@ import Navbar from '@/components/global/Navbar';
 
 export default function IndexPage({ children }: { children: React.ReactNode }) {
    return (
-      <>
-         <Navbar />
-         {/* Using <main> for the main content */}
-         <main className="flex-grow bg-gray-200">
-            {/* Suspense will display the fallback while children (pages/components) are loading */}
-            <Suspense
-               fallback={
-                  <div className="flex justify-center items-center min-h-screen">
-                     <p>Loading...</p>
-                  </div>
-               }
-            >
-               {children}
-            </Suspense>
-         </main>
-         {/* Assuming Footer component contains <footer> tag */}
-         <Footer />
-      </>
+      <html lang="en">
+         <body>
+            <Navbar />
+            <main className="flex-grow bg-gray-200">
+               <Suspense
+                  fallback={
+                     <div className="flex justify-center items-center min-h-screen">
+                        <p>Loading...</p>
+                     </div>
+                  }
+               >
+                  {children}
+               </Suspense>
+            </main>
+            <Footer />
+         </body>
+      </html>
    );
 }
