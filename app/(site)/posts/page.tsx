37209aa502f50e41044081e0b55fb8999fa3595e
RD <raymond.luke.spartz@gmail.com>
2023-12-14 13:41:33 -0800

    remove from layout and add to posts and posts[slug] page directly

--- app/(site)/posts/page.tsx
+++ app/(site)/posts/page.tsx
@@ -1,13 +1,18 @@
 import { loadPosts } from "@/sanity/loader/loadQuery";
 import { PostsList } from "@/components/global/PostsList";
+import Loading from "./loading";
+
+import { Suspense } from "react";
 
 export default async function PostPage() {
 	const response = await loadPosts();
 	const post = response.data;
 
 	return (
-		<div className="flex w-full">
-			<PostsList post={post} />
-		</div>
+		<Suspense fallback={<Loading />}>
+			<div className="flex w-full">
+				<PostsList post={post} />
+			</div>
+		</Suspense>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
 			>
 				<body>
 					<Navbar pageBackground="light" />
-					<Suspense fallback={<Loading />}>{children}</Suspense>
+					{children}
 					<Footer />
 					<Analytics />
 				</body>
