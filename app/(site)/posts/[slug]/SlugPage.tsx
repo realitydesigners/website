d43e8fd2c1d9acb08f8b464bab7ef501bdc9a725
RD <raymond.luke.spartz@gmail.com>
2023-12-13 21:43:57 -0800

    new slug rendering

--- app/(site)/posts/[slug]/SlugPage.tsx
+++ app/(site)/posts/[slug]/SlugPage.tsx
@@ -10,57 +10,46 @@ export interface PageProps {
 	encodeDataAttribute?: EncodeDataAttributeCallback;
 }
 
-const Page: React.FC<PageProps> = ({ data, encodeDataAttribute }) => {
-	//console.log(data);
+const Page: React.FC<PageProps> = ({ data }) => {
+	const renderHeadingBlock = (block, key) => {
+		return block.layout === "dark" ? (
+			<HeadingBlockDark key={key} block={block} />
+		) : (
+			<HeadingBlockLight key={key} block={block} />
+		);
+	};
 
-	const { block } = data ?? {};
+	const renderContentBlock = (content, key) => {
+		if (!content.content) return null;
+		return content.layout === "dark" ? (
+			<div className="w-full h-auto bg-black lg:pt-24" key={key}>
+				<PortableTextComponent content={content.content} template="dark" />
+			</div>
+		) : content.layout === "light" ? (
+			<div className="w-full h-auto bg-gray-200 lg:pt-24" key={key}>
+				<PortableTextComponent content={content.content} template="light" />
+			</div>
+		) : null;
+	};
+
+	const renderTeamBlock = (block, key) =>
+		block._type === "teamBlock" && <TeamBlock key={key} block={block} />;
 
 	return (
 		<main>
-			{/* HEADING BLOCK */}
-			{block?.map((block, index) => {
-				if (block._type === "headingBlock") {
-					return block.layout === "dark" ? (
-						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-						<HeadingBlockDark key={index} block={block} />
-					) : (
-						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-						<HeadingBlockLight key={index} block={block} />
-					);
+			{data?.block?.map((block, index) => {
+				const key = index; // Prefer unique identifier, fallback to index
+				switch (block._type) {
+					case "headingBlock":
+						return renderHeadingBlock(block, key);
+					case "contentBlock":
+						return renderContentBlock(block, key);
+					case "teamBlock":
+						return renderTeamBlock(block, key);
+					default:
+						return null;
 				}
-				return null;
 			})}
-
-			{/* CONTENT BLOCK */}
-			{data?.block?.map(
-				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-				(contentBlock: any, index: number) =>
-					contentBlock?.content &&
-					(contentBlock.layout === "dark" ? (
-						<div className="w-full h-auto bg-black lg:pt-24">
-							<PortableTextComponent
-								content={contentBlock.content}
-								template="dark"
-							/>
-						</div>
-					) : contentBlock.layout === "light" ? (
-						<div className="w-full h-auto bg-gray-200  lg:pt-24">
-							<PortableTextComponent
-								content={contentBlock.content}
-								template="light"
-							/>
-						</div>
-					) : null),
-			)}
-			{/* CONTENT BLOCK */}
-			{data?.block?.map((block, index) => (
-				<div>
-					{block._type === "teamBlock" && (
-						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-						<TeamBlock key={index} block={block} />
-					)}
-				</div>
-			))}
 		</main>
 	);
 };
