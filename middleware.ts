0f113bf39b232e6e2b753161a0e41c75795e975f
RD <raymond.luke.spartz@gmail.com>
2023-12-14 12:45:48 -0800

    add route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-   publicRoutes: ['/', '/story', '/team', '/videos', '/posts', '/posts/:slug*', '/videos/:slug*', '/team/:slug*'],
+   publicRoutes: ['/', '/story', '/team', '/videos', '/posts', '/api/send', '/posts/:slug*', '/videos/:slug*', '/team/:slug*'],
 });
 
 export const config = {
