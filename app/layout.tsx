01936fe0b7c22360875d69d80cfa793d28d765e1
RD <raymond.luke.spartz@gmail.com>
2023-12-15 12:44:39 -0800

    custom sign in - signup pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { staatliches, jura } from "@/fonts";
 
-export default async function VideosLayout({
+export default async function Layout({
 	children,
 }: { children: React.ReactNode }) {
 	return (
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,15 @@
+import "@/app/global.css";
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-	return <SignIn />;
+export default function SignInPage() {
+	return (
+		<div className="w-full flex lg:flex-row flex-col h-screen">
+			<div className="hidden bg-gray-400 p-2 h-auto justify-center items-center  lg:flex lg:w-1/2">
+				<p className="hidden">Animation Goes Here</p>
+			</div>
+			<div className="w-full flex justify-center items-center h-screen lg:h-auto  lg:pr-6 bg-gray-300 lg:w-1/2">
+				<SignIn />
+			</div>
+		</div>
+	);
 }
