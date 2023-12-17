bf126aed8a97c4bb77e1c4c96dc039a425ea0059
RD <raymond.luke.spartz@gmail.com>
2023-12-17 14:40:12 -0800

    new

--- sanity/lib/api.ts
+++ sanity/lib/api.ts
@@ -11,7 +11,7 @@ export const projectId = assertValue(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-06-21';
 
 // See the app/api/revalidate/route.ts for how this is used
-export const revalidateSecret = 'jsnco21aej';
+export const revalidateSecret = process.env.SANITY_REVALIDATE_SECRET;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
    if (v === undefined) {
