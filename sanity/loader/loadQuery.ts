77c2a2bd7ebd0b14d5f7403a7249d59759833dbd
RD <raymond.luke.spartz@gmail.com>
2023-12-17 14:26:33 -0800

    npm

--- sanity/loader/loadQuery.ts
+++ sanity/loader/loadQuery.ts
@@ -1,7 +1,7 @@
 import 'server-only';
 import { draftMode } from 'next/headers';
 import { client } from '@/sanity/lib/client';
-import { homePageQuery, pagesBySlugQuery, settingsQuery, postsQuery, postsBySlugQuery, categoryQuery, categoryBySlugQuery, getVideosQuery, getVideoBySlugQuery, teamQuery, teamBySlugQuery } from '@/sanity/lib/queries';
+import { settingsQuery, postsQuery, postsBySlugQuery, categoryQuery, categoryBySlugQuery, getVideosQuery, getVideoBySlugQuery, teamQuery, teamBySlugQuery } from '@/sanity/lib/queries';
 import { token } from '@/sanity/lib/token';
 import { SettingsPayload, PostsPayload, CategoryPayload, VideoPayload, TeamPayload } from '@/types';
 import * as queryStore from '@sanity/react-loader';
@@ -9,17 +9,10 @@ import * as queryStore from '@sanity/react-loader';
 const serverClient = client.withConfig({
    token,
    stega: {
-      // Enable stega if it's a Vercel preview deployment, as the Vercel Toolbar has controls that shows overlays
       enabled: process.env.VERCEL_ENV === 'preview',
    },
 });
 
-/**
- * Sets the server client for the query store, doing it here ensures that all data fetching in production
- * happens on the server and not on the client.
- * Live mode in `sanity/presentation` still works, as it uses the `useLiveMode` hook to update `useQuery` instances with
- * live draft content using `postMessage`.
- */
 queryStore.setServerClient(serverClient);
 
 const usingCdn = serverClient.config().useCdn;
@@ -32,7 +25,7 @@ export const loadQuery = ((query, params = {}, options = {}) => {
    if (!usingCdn && Array.isArray(options.next?.tags)) {
       revalidate = false;
    } else if (usingCdn) {
-      revalidate = 1;
+      revalidate = 60;
    }
    return queryStore.loadQuery(query, params, {
       ...options,
@@ -41,8 +34,6 @@ export const loadQuery = ((query, params = {}, options = {}) => {
          ...(options.next || {}),
       },
       perspective,
-      // @TODO add support in `@sanity/client/stega` for the below
-      // stega: {enabled: draftMode().isEnabled}
    });
 }) satisfies typeof queryStore.loadQuery;
 
