0d3c387058367a5a22def1aa372502e651c4dc3f
RD <raymond.luke.spartz@gmail.com>
2023-12-18 13:56:58 -0800

    improve styles of the internal link

--- components/blocks/InternalLink.jsx
+++ components/blocks/InternalLink.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, Suspense } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { jura, staatliches } from '@/fonts';
 import { getPostData } from '@/app/api/actions/fetchInternalLink';
@@ -48,26 +48,28 @@ export const ArticlePreviewDialog = ({ isOpen, onClose, postData }) => {
    const firstBlock = postData?.block?.[0];
 
    return (
-      <div id="popup" className="flex flex-col my-5 items-center justify-center ">
-         <div className="w-full relative max-w-3xl h-[50vh] overflow-auto p-4 bg-gray-300 shadow-lg rounded-lg flex flex-col">
+      <div id="popup" className="flex flex-col my-5 items-center justify-center">
+         <div className="w-full justify-center relative  h-[50vh] lg:h-[33vh] overflow-auto p-4 bg-gray-300 shadow-lg rounded-lg grid grid-cols-1 lg:grid-cols-2 gap-4">
             {firstBlock && (
                <>
                   {firstBlock.image && (
-                     <div className="relative mb-4">
+                     <div className="relative mb-4 lg:mb-0">
                         <ImageBox image={firstBlock.image} alt={`Cover Image for ${firstBlock.heading}`} classesWrapper="h-[250px] w-full transform rounded-[.5em] object-cover transition-transform duration-300 group-hover:scale-110" />
                      </div>
                   )}
 
-                  <Link href={`/posts/${postData.slug.current}`} className={`${staatliches.className} text-black font-bold text-4xl mb-2`}>
-                     {firstBlock.heading}
-                  </Link>
+                  <div>
+                     <Link href={`/posts/${postData.slug.current}`} className={`${staatliches.className} text-black font-bold text-4xl mb-2`}>
+                        {firstBlock.heading}
+                     </Link>
 
-                  {firstBlock.subheading && <p className={`${jura.className} text-black text-lg mb-4`}>{firstBlock.subheading}</p>}
+                     {firstBlock.subheading && <p className={`${jura.className} text-black text-lg mb-4`}>{firstBlock.subheading}</p>}
+                  </div>
                </>
             )}
 
-            <div className="flex justify-between items-center">
-               <Link href={`/posts/${postData.slug.current}`} className="w-full lg:w-1/2 flex justify-center items-center px-6 py-2 bg-black text-white font-bold rounded hover:bg-black/75 transition-colors">
+            <div className="flex w-full justify-between items-center col-span-1 lg:col-span-2">
+               <Link href={`/posts/${postData.slug.current}`} className="w-full flex justify-center items-center px-6 py-2 bg-black text-white font-bold rounded hover:bg-black/75 transition-colors">
                   Read Now
                </Link>
             </div>
@@ -85,7 +87,7 @@ export const ArticlePreviewDialog = ({ isOpen, onClose, postData }) => {
 
 const LoadingIndicator = () => (
    <div className="flex my-4 items-center justify-center">
-      <div className="w-full max-w-3xl h-[50vh] overflow-auto flex justify-center items-center p-4 bg-gray-300 shadow-lg rounded-lg">
+      <div className="w-full max-w-3xl h-[50vh] lg:h-[33vh]  overflow-auto flex justify-center items-center p-4 bg-gray-300 shadow-lg rounded-lg">
          {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
          <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
             <circle cx="25" cy="25" r="20" stroke="#888888" strokeWidth="5" fill="none" strokeDasharray="31.415, 31.415" strokeDashoffset="0">
