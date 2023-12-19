f300740aaa353f7b05ad7d9ddcaaa595cbd7d851
RD <raymond.luke.spartz@gmail.com>
2023-12-18 19:51:07 -0800

    new test img loading

--- components/global/Images.tsx
+++ components/global/Images.tsx
@@ -1,8 +1,7 @@
 "use client";
 import Image from "next/image";
 import { urlForImage } from "@/sanity/lib/utils";
-import { jura, staatliches } from "@/fonts";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function SanityImage({
 	image,
@@ -14,38 +13,50 @@ export function SanityImage({
 	...props
 }) {
 	const [isLoading, setIsLoading] = useState(true);
+	const [hasError, setHasError] = useState(false);
 	const imageUrl =
 		image && urlForImage(image)?.height(height).width(width).fit("crop").url();
 
+	// If the image fails to load, set an error flag
+	useEffect(() => {
+		if (imageUrl) {
+			const img = document.createElement("img");
+			img.src = imageUrl;
+			img.onload = () => setIsLoading(false);
+			img.onerror = () => {
+				setIsLoading(false);
+				setHasError(true);
+			};
+		}
+	}, [imageUrl]);
+
 	return (
 		<div
-			className={`w-full overflow-hidden ${classesWrapper}`}
+			className={`relative w-full overflow-hidden ${classesWrapper}`}
 			data-sanity={props["data-sanity"]}
 		>
-			{isLoading && <LoadingIndicator />}
-			{imageUrl && (
+			{isLoading && !hasError && <LoadingIndicator />}
+			{imageUrl && !hasError && (
 				<Image
 					priority={priority}
 					className="object-cover cover h-full w-full"
 					alt={alt}
-					width={width}
-					height={height}
+					width={width || undefined} // width and height can be undefined to let CSS handle sizing
+					height={height || undefined}
 					src={imageUrl}
-					onLoad={() => setIsLoading(false)}
+					placeholder="empty" // This can be "blur" if you provide a blurDataURL
 				/>
 			)}
+			{hasError && <p>Image failed to load.</p>}
 		</div>
 	);
 }
+
 const LoadingIndicator = () => (
-	<div className="flex items-center justify-center w-full h-full">
-		<div className="flex flex-col items-center justify-center">
-			<div className="animate-pulse w-16 h-10 bg-gray-300 rounded" />
-			<p
-				className={`${staatliches.className} text-gray-400 font-mono text-sm uppercase tracking-widest mt-2`}
-			>
-				Loading...
-			</p>
-		</div>
+	<div className="absolute inset-0 bg-gray-300 flex items-center justify-center">
+		<div className="animate-pulse w-16 h-10 bg-gray-300 rounded" />
+		<p className="text-gray-400 font-mono text-sm uppercase tracking-widest mt-2">
+			Loading...
+		</p>
 	</div>
 );
