653f9c1718023a98b509e0db55db57317266807f
RD <raymond.luke.spartz@gmail.com>
2023-12-22 09:53:25 -0800

    added gsap into /effects

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -6,36 +6,15 @@ import { PostsPayload, VideoPayload, Image } from "@/types";
 import PostItem from "@/components/feed/PostItem";
 import VideoItem from "@/components/feed/VideoItem";
 import ImageItem from "@/components/feed/ImageItem";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-// Register ScrollTrigger only once
-gsap.registerPlugin(ScrollTrigger);
+import { useAnimateOnScroll } from "@/components/effects/useAnimateOnScroll";
 
 type FeedItem = PostsPayload | VideoPayload | Image;
-
-const animateFeedItems = () => {
-	const targets = document.querySelectorAll(".feed-item");
-	for (const item of targets) {
-		gsap.from(item, {
-			duration: 1.5, // Duration of the animation
-			opacity: 0, // Start with an opacity of 0
-			y: 50, // Start 50 pixels down from the final position
-			ease: "expo.out", // Use a more dramatic easing for smoother effect
-			scrollTrigger: {
-				trigger: item,
-				start: "top bottom-=100", // Start the animation sooner
-				end: "bottom top+=100", // End the animation later
-				scrub: 1.5, // Increase the scrub duration for smoother effect
-				// markers: true,          // Uncomment to see markers for debugging
-			},
-		});
-	}
-};
 
 export default function FeedPage() {
 	const [items, setItems] = useState<FeedItem[]>([]);
 	const containerRef = useRef<HTMLDivElement | null>(null);
+
+	useAnimateOnScroll(".feed-item", {}, process.env.NODE_ENV === "development");
 
 	useEffect(() => {
 		async function fetchData() {
@@ -48,12 +27,6 @@ export default function FeedPage() {
 
 		fetchData();
 	}, []);
-
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		const timeoutId = setTimeout(animateFeedItems, 500); // Set a timeout to ensure elements are rendered
-		return () => clearTimeout(timeoutId); // Clear the timeout if the component unmounts
-	}, [items]);
 
 	return (
 		<div
