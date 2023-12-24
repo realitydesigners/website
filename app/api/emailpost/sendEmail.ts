a1f57aa646c00dcf75f04501923a2d55813401e0
RD <raymond.luke.spartz@gmail.com>
2023-12-24 01:37:33 -0800

    email test

---
+++ app/api/emailpost/sendEmail.ts
@@ -0,0 +1,36 @@
+import { NewPost } from "@/components/email/new-post";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request) {
+	try {
+		const postData = await request.json();
+		const heading = postData.heading || "New Blog Post";
+		const subheading = postData.subheading || "No Subheading";
+		const slug = postData.slug || "no-slug";
+
+		const emailContent = NewPost({
+			heading,
+			slug,
+			subheading,
+		});
+
+		const recipients = ["raymond.luke.spartz@gmail.com"]; // Replace with subscriber list
+
+		const { data, error } = await resend.emails.send({
+			from: "Reality Designers <info@realitydesigners.tv>",
+			to: recipients,
+			subject: `New Blog Post: ${heading}`,
+			react: emailContent as React.ReactElement,
+		});
+
+		if (error) {
+			return Response.json({ error });
+		}
+
+		return Response.json({ data });
+	} catch (error) {
+		return Response.json({ error });
+	}
+}
---
+++ components/email/new-post.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import {
+	Body,
+	Button,
+	Head,
+	Heading,
+	Html,
+	Section,
+	Tailwind,
+	Text,
+} from "@react-email/components";
+
+interface EmailTemplateProps {
+	heading: string;
+	subheading: string;
+	slug: string;
+}
+
+export const NewPost: React.FC<Readonly<EmailTemplateProps>> = ({
+	heading,
+	subheading,
+	slug,
+}) => (
+	<Html>
+		<Head />
+		<Tailwind>
+			<Body className="my-auto mx-auto font-sans">
+				<Section className="text-center py-6">
+					<Heading className="text-black text-[24px] font-bold">
+						{heading}
+					</Heading>
+					<Text className="text-black text-[16px] mt-4">{subheading}</Text>
+				</Section>
+				<Section className="text-center py-6">
+					<Button
+						href={`https://www.realitydesigners.tv/posts/${slug}`}
+						style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+					>
+						Read the Full Post
+					</Button>
+				</Section>
+				<Section className="text-center py-6">
+					<Text className="text-black text-[16px] leading-[24px]">
+						Stay engaged with our latest content and updates. Follow us on
+						Instagram for more insights and discussions.
+					</Text>
+					<Button
+						href="https://www.instagram.com/realitydesignerstv/"
+						style={{
+							background: "#000",
+							color: "#fff",
+							padding: "12px 20px",
+							marginTop: "20px",
+						}}
+					>
+						Follow on Instagram
+					</Button>
+				</Section>
+			</Body>
+		</Tailwind>
+	</Html>
+);
