5ff197c0556194dc5b66c9ef995f017fb368a392
RD <raymond.luke.spartz@gmail.com>
2023-12-24 03:14:53 -0800

    add

--- app/api/emailpost/route.ts
+++ app/api/emailpost/route.ts
@@ -5,6 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
 	try {
+		// Check if the incoming request has the expected "application/json" content type
 		if (request.headers.get("content-type") !== "application/json") {
 			throw new Error("Invalid Content-Type. Expected application/json");
 		}
@@ -22,16 +23,16 @@ export async function POST(request) {
 
 		const recipients = ["raymond.luke.spartz@gmail.com"]; // Replace with subscriber list
 
-		// Define the email message with relevant content
-		const emailMessage = {
+		// Send the email with email-specific headers
+		const { data, error } = await resend.emails.send({
 			from: "Reality Designers <info@realitydesigners.tv>",
 			to: recipients,
 			subject: `New Blog Post: ${heading}`,
 			react: emailContent as React.ReactElement,
-		};
-
-		// Send the email
-		const { data, error } = await resend.emails.send(emailMessage);
+			headers: {
+				"X-Entity-Ref-ID": "Sh7Vh29H87sad",
+			},
+		});
 
 		if (error) {
 			return Response.json({ error });
