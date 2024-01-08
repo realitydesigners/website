1122cb60b87e397db2e1501224586eace34477bd
RD <raymond.luke.spartz@gmail.com>
2024-01-08 10:56:31 -0800

    rm loading

--- app/team/layout.tsx
+++ app/team/layout.tsx
@@ -1,8 +1,7 @@
 import Footer from "@/components/global/Footer";
 import Navbar from "@/components/global/Navbar";
-import { Suspense } from "react";
+
 import "tailwindcss/tailwind.css";
-import Loading from "./loading";
 
 export default async function TeamLayout({
 	children,
@@ -14,7 +13,7 @@ export default async function TeamLayout({
 	return (
 		<div className="flex min-h-screen flex-col bg-gray-200">
 			<Navbar pageBackground="light" />
-			<Suspense fallback={<Loading />}>{children}</Suspense>
+			{children}
 			<Footer />
 		</div>
 	);
