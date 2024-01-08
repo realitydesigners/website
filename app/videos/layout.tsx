dfd0fd8fd4057fac6c68853509fee5b3063b4a53
RD <raymond.luke.spartz@gmail.com>
2024-01-08 10:59:19 -0800

    all loading removed

--- app/videos/layout.tsx
+++ app/videos/layout.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/global/Footer";
 import Navbar from "@/components/global/Navbar";
 import { Suspense } from "react";
 import "tailwindcss/tailwind.css";
-import Loading from "./loading";
 
 export default async function PostsLayout({
 	children,
@@ -12,7 +11,7 @@ export default async function PostsLayout({
 	return (
 		<div className="w-screen h-auto flex flex-col bg-gray-200">
 			<Navbar pageBackground="light" />
-			<Suspense fallback={<Loading />}>{children}</Suspense>
+			{children}
 			<Footer />
 		</div>
 	);
--- next.lock/layout.tsx
+++ next.lock/layout.tsx
@@ -1,13 +1,15 @@
 import Footer from "@/components/global/Footer";
 import Navbar from "@/components/global/Navbar";
-
+import { Suspense } from "react";
 import "tailwindcss/tailwind.css";
 
-export default async function StoryLayout({
+export default async function LibraryLayout({
 	children,
-}: { children: React.ReactNode }) {
+}: {
+	children: React.ReactNode;
+}) {
 	return (
-		<div className="flex min-h-screen flex-col bg-gray-200">
+		<div className="w-screen h-screen bg-gray-200">
 			<Navbar pageBackground="light" />
 			{children}
 			<Footer />
