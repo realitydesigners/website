4ffb642cf143815039fe03a7d00ac5b956b74870
RD <raymond.luke.spartz@gmail.com>
2024-02-19 10:31:59 -0800

    improve ui for HeadingBlock

--- components/blocks/sectionBlocks/HeadingBlock.tsx
+++ components/blocks/sectionBlocks/HeadingBlock.tsx
@@ -1,34 +1,8 @@
 "use client";
 import { SanityImage } from "@/components/global/Images";
-import { monomaniac, play } from "@/fonts";
+import { monomaniac, play, space } from "@/fonts";
 import Link from "next/link";
 import React from "react";
-
-import { TemplateTheme, ThemeProps } from "@/components/blocks/types";
-
-const themeClasses: Record<TemplateTheme, ThemeProps> = {
-	light: {
-		containerBg: "bg-gray-200",
-		containerBorder: "border border-gray-600/50",
-		textColor: "text-black",
-		tagBg: "bg-black",
-		tagText: "text-gray-200",
-	},
-	dark: {
-		containerBg: "bg-black",
-		containerBorder: " border border-gray-600/50",
-		textColor: "text-gray-200",
-		tagBg: "bg-gray-200",
-		tagText: "text-black",
-	},
-	transparent: {
-		containerBg: "bg-transparent",
-		containerBorder: "",
-		textColor: "text-gray-200",
-		tagBg: "bg-gray-200",
-		tagText: "text-black",
-	},
-};
 
 const getPublicationDate = (block) => {
 	let publicationDate = block.publicationDate;
@@ -49,15 +23,17 @@ const getPublicationDate = (block) => {
 		: "Date not available";
 };
 
-const RenderCategory = ({ category, style }) => {
+const RenderCategory = ({ category }) => {
 	if (!category) return null;
 
 	return (
-		<span
-			className={`${monomaniac.className} text-xs h-auto flex uppercase font-mono font-semibold ${style.tagBg} items-center justify-center p-1 pl-2 pr-2 tracking-widest whitespace-nowrap ${style.tagText} `}
-		>
-			{category.title}
-		</span>
+		<div className="flex items-center justify-center bg-gradient-to-r from-blue-100/100 to-blue-100/90 p-1 my-1 pl-2 pr-2">
+			<span
+				className={`${monomaniac.className} text-xs h-auto flex uppercase font-mono text-black font-semibold items-center justify-center tracking-widest whitespace-nowrap `}
+			>
+				{category.title}
+			</span>
+		</div>
 	);
 };
 
@@ -79,13 +55,11 @@ const TeamSection = ({ team, theme }) => {
 								width={100}
 								height={100}
 								priority={true}
-								classesWrapper="h-[50px] w-[50px] object-cover cover rounded-[2em]"
+								classesWrapper="h-[40px] w-[40px] object-cover cover rounded-[2em]"
 								theme={theme} // Pass theme to SanityImage
 							/>
-							<span
-								className={`ml-2 uppercase tracking-wide text-sm ${themeClasses[theme].textColor}`}
-							>
-								{team.name}
+							<span className="ml-2 uppercase tracking-wide text-sm  font-bold uppercase leading-none cursor-pointer bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text ">
+								Written by {team.name}
 							</span>
 						</div>
 					)}
@@ -96,81 +70,152 @@ const TeamSection = ({ team, theme }) => {
 };
 
 const HeadingBlock = ({ block }) => {
-	if (block?._type !== "headingBlock") {
-		return null;
-	}
-
 	const { className } = block;
-	const style = themeClasses[className];
 	const formattedDate = getPublicationDate(block);
+	const theme = block.className;
 
-	return (
-		<div className={`w-full h-auto ${style.containerBg} pt-20 lg:pt-32`}>
-			<div className="w-full flex justify-center flex-wrap">
-				<div
-					className={`w-11/12 flex flex-wrap flex-cols items-center justify-between lg:hidden ${style.textColor}`}
-				>
-					<span
-						className={`${monomaniac.className} ml-2 uppercase w-auto text-xs font-mono tracking-widest`}
-					>
-						POSTED ON {formattedDate}
-					</span>
-
-					{block.category && (
-						<RenderCategory category={block.category} style={style} />
-					)}
-				</div>
-
-				{block.image && (
-					<div className="w-full flex-wrap lg:w-1/2 p-4 flex">
-						<div className={" w-full h-full object-cover object-contain "}>
-							<SanityImage
-								image={block.image}
-								width={1000}
-								height={1000}
-								priority={true}
-								alt={`Cover Image for ${block.title}`}
-								classesWrapper={`h-full w-full rounded-[1em] ${style.containerBorder} `}
-								theme={className}
-							/>
-						</div>
-					</div>
-				)}
-				<div className="w-full lg:w-1/2 p-4 pr-4 lg:pr-20 flex pt-2 lg:pt-4 justify-center flex-cols">
-					<div className="w-full">
+	switch (theme) {
+		case "dark":
+			return (
+				<div className={"w-full h-auto pt-16 lg:pt-32"}>
+					<div className="w-full flex justify-center flex-wrap">
 						<div
-							className={`w-full hidden justify-between items-center lg:flex mb-6 ${style.textColor}`}
+							className={
+								"w-11/12 flex flex-wrap   flex-cols items-center justify-between lg:hidden"
+							}
 						>
+							{block.category && <RenderCategory category={block.category} />}
 							<span
-								className={`${monomaniac.className} uppercase font-mono w-auto text-xs tracking-widest`}
+								className={`${monomaniac.className} ml-2 my-1 uppercase w-auto text-xs bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text  tracking-widest`}
 							>
-								POSTED {formattedDate}
+								POSTED ON {formattedDate}
 							</span>
-
-							{block.category && (
-								<RenderCategory category={block.category} style={style} />
-							)}
 						</div>
-						{block.heading && (
-							<h1
-								className={`${monomaniac.className} text-5xl ${style.textColor} capitalize leading-tightest mb-4 lg:text-6xl`}
-							>
-								{block.heading}
-							</h1>
+
+						{block.image && (
+							<div className="w-full flex-wrap lg:w-1/2 p-4 flex">
+								<div className={" w-full h-full object-cover object-contain "}>
+									<SanityImage
+										image={block.image}
+										width={1000}
+										height={1000}
+										priority={true}
+										alt={`Cover Image for ${block.title}`}
+										classesWrapper="h-full w-full"
+										theme={className}
+									/>
+								</div>
+							</div>
 						)}
-						{block.subheading && (
-							<p
-								className={`${play.className} w-full text-2xl ${style.textColor} leading-7 tracking-wide`}
-							>
-								{block.subheading}
-							</p>
-						)}
-						<TeamSection team={block.team} theme={className} />
+						<div className="w-full lg:w-1/2 p-4 pr-4 lg:pr-20 flex pt-2 lg:pt-4 justify-center flex-cols">
+							<div className="w-full">
+								<div className="w-full hidden justify-between items-center lg:flex mb-6">
+									{block.category && (
+										<RenderCategory category={block.category} />
+									)}
+									<span
+										className={`${monomaniac.className} uppercase bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text w-auto text-xs tracking-widest`}
+									>
+										POSTED {formattedDate}
+									</span>
+								</div>
+								{block.heading && (
+									<h1
+										className={`${space.className} p-1 text-[9vw] lg:text-[4vw] font-bold uppercase leading-none cursor-pointer bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text`}
+									>
+										{block.heading}
+									</h1>
+								)}
+								{block.subheading && (
+									<p
+										className={`${space.className} p-1 text-xl leading-tight  bg-gradient-to-r from-blue-100/50 to-blue-100/50 text-transparent bg-clip-text `}
+									>
+										{block.subheading}
+									</p>
+								)}
+								<TeamSection team={block.team} theme={className} />
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	);
+			);
+		case "light":
+			return (
+				<div className="w-full h-auto bg-gray-200 pt-20 lg:pt-32">
+					{/* Light theme layout */}
+				</div>
+			);
+		case "transparent":
+			return (
+				<div className="w-full h-auto bg-transparent pt-20 lg:pt-32">
+					{/* Transparent theme layout */}
+				</div>
+			);
+		default:
+			return (
+				<div className={"w-full h-auto pt-16 lg:pt-32"}>
+					<div className="w-full flex justify-center flex-wrap">
+						<div
+							className={
+								"w-11/12 flex flex-wrap   flex-cols items-center justify-between lg:hidden"
+							}
+						>
+							{block.category && <RenderCategory category={block.category} />}
+							<span
+								className={`${monomaniac.className} ml-2 my-1 uppercase w-auto text-xs bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text  tracking-widest`}
+							>
+								POSTED ON {formattedDate}
+							</span>
+						</div>
+
+						{block.image && (
+							<div className="w-full flex-wrap lg:w-1/2 p-4 flex">
+								<div className={" w-full h-full object-cover object-contain "}>
+									<SanityImage
+										image={block.image}
+										width={1000}
+										height={1000}
+										priority={true}
+										alt={`Cover Image for ${block.title}`}
+										classesWrapper="h-full w-full"
+										theme={className}
+									/>
+								</div>
+							</div>
+						)}
+						<div className="w-full lg:w-1/2 p-4 pr-4 lg:pr-20 flex pt-2 lg:pt-4 justify-center flex-cols">
+							<div className="w-full">
+								<div className="w-full hidden justify-between items-center lg:flex mb-6">
+									{block.category && (
+										<RenderCategory category={block.category} />
+									)}
+									<span
+										className={`${monomaniac.className} uppercase bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text w-auto text-xs tracking-widest`}
+									>
+										POSTED {formattedDate}
+									</span>
+								</div>
+								{block.heading && (
+									<h1
+										className={`${space.className} p-1 text-[9vw] lg:text-[4vw] font-bold uppercase leading-none cursor-pointer bg-gradient-to-r from-blue-100/100 to-blue-100/90 text-transparent bg-clip-text`}
+									>
+										{block.heading}
+									</h1>
+								)}
+								{block.subheading && (
+									<p
+										className={`${space.className} p-1 text-xl leading-tight  bg-gradient-to-r from-blue-100/50 to-blue-100/50 text-transparent bg-clip-text `}
+									>
+										{block.subheading}
+									</p>
+								)}
+								<TeamSection team={block.team} theme={className} />
+							</div>
+						</div>
+					</div>
+				</div>
+			);
+	}
 };
 
 export default React.memo(HeadingBlock);
