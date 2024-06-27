2688e8021fdd995603134902524b6df5b48ce659
Raymond <raymond.luke.spartz@gmail.com>
2024-06-27 16:32:58 -0500

    dep, add use client to sanity config

--- app/(admin)/api/actions/fetchInternalLink.tsx
+++ app/(admin)/api/actions/fetchInternalLink.tsx
@@ -3,6 +3,6 @@
 import { sanityFetch } from "@/sanity/lib/client";
 import { postsBySlugQuery } from "@/sanity/lib/queries";
 
-export async function getPostData(slug) {
+export async function getPostData(slug: string) {
 	return sanityFetch({ query: postsBySlugQuery, qParams: { slug }, tags: [] });
 }
