84823e1e7be088d8256edf14d2ba5b58df0b43ec
Raymond <raymond.luke.spartz@gmail.com>
2024-07-02 14:09:31 -0500

    more spline

--- app/(user)/test/SplineScene.jsx
+++ app/(user)/test/SplineScene.jsx
@@ -1,24 +1,21 @@
 import Spline from '@splinetool/react-spline';
-// SplineScene.jsx
 import React from 'react';
 
-const SplineScene = ({ onSplineMouseDown }) => {
-    function handleSplineMouseDown(e) {
-        // Call onSplineMouseDown with the event
-        onSplineMouseDown(e);
-    }
+const SplineScene = ({ onObjectClicked }) => {
+ 
+    const objects = [
+        { id: '82833606-2348-48d4-b846-5a8e2ea948cd', name: 'Object 1' },
+        { id: 'f0d4c5a8-b2b8-4b98-968a-a31b775ef9bf', name: 'Object 2' },
+    ];
 
     function onLoad(spline) {
-        try {
-            const obj = spline.findObjectById('82833606-2348-48d4-b846-5a8e2ea948cd');
-            if (obj) {
-                console.log('Found object by ID:', obj);
-            } else {
-                console.log('Object with ID 82833606-2348-48d4-b846-5a8e2ea948cd not found in the Spline scene.');
+            for (let i = 0; i < objects.length; i++) {
+                const obj = objects[i];
+                const object = spline.findObjectById(obj.id);
+                if (object) {
+                    object.addEventListener('mousedown', () => onObjectClicked(obj.id));
+                } 
             }
-        } catch (error) {
-            console.error('Error loading Spline scene:', error);
-        }
     }
 
     return (
@@ -26,7 +23,6 @@ const SplineScene = ({ onSplineMouseDown }) => {
             <Spline
                 scene="https://prod.spline.design/KWwl1PhEzFMKj770/scene.splinecode" // Replace with your actual scene URL
                 onLoad={onLoad}
-                onSplineMouseDown={handleSplineMouseDown} // Pass click handler
             />
         </main>
     );
