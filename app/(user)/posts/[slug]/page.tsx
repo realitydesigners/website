dd0a802b6d0bf82567696e1494d1c254d850fb16
raymond <raymond.luke.spartz@gmail.com>
2024-07-05 19:53:27 -0500

    test commit

--- app/(user)/posts/[slug]/page.tsx
+++ app/(user)/posts/[slug]/page.tsx
@@ -11,103 +11,101 @@ import { Metadata, ResolvingMetadata } from "next";
 import React, { Suspense, useMemo } from "react";
 
 type Props = {
-    params: { slug: string };
+	params: { slug: string };
 };
 
 export function generateStaticParams() {
-    return generateStaticSlugs("posts");
+	return generateStaticSlugs("posts");
 }
 
 export async function generateMetadata(
-    { params }: Props,
-    parent: ResolvingMetadata,
+	{ params }: Props,
+	parent: ResolvingMetadata,
 ): Promise<Metadata> {
-    const metadataBaseUrl =
-        process.env.NEXT_PUBLIC_METADATA_BASE || "http://localhost:3000";
-    const post = await sanityFetch<PostsPayload>({
-        query: postsBySlugQuery,
-        tags: ["post"],
-        qParams: { slug: params.slug },
-    });
-    //@ts-ignore
-    const ogImageUrl =
-        post?.block?.[0]?.imageRef?.imageUrl ||
-        (Array.isArray(post?.block?.[0]?.imageRef) &&
-            (post?.block?.[0]?.imageRef[0]?.imageUrl ||
-                post?.block?.[0]?.imageRef[0]?.image)) ||
-        post?.block?.[0]?.imageRef?.image;
+	const metadataBaseUrl =
+		process.env.NEXT_PUBLIC_METADATA_BASE || "http://localhost:3000";
+	const post = await sanityFetch<PostsPayload>({
+		query: postsBySlugQuery,
+		tags: ["post"],
+		qParams: { slug: params.slug },
+	});
+	//@ts-ignore
+	const ogImageUrl =
+		post?.block?.[0]?.imageRef?.imageUrl ||
+		(Array.isArray(post?.block?.[0]?.imageRef) &&
+			(post?.block?.[0]?.imageRef[0]?.imageUrl ||
+				post?.block?.[0]?.imageRef[0]?.image)) ||
+		post?.block?.[0]?.imageRef?.image;
 
-    const ogImageAlt =
-        post?.block?.[0]?.imageRef?.imageAlt || "Your default alt text";
+	const ogImageAlt =
+		post?.block?.[0]?.imageRef?.imageAlt || "Your default alt text";
 
-    const metadataBase = new URL(metadataBaseUrl);
+	const metadataBase = new URL(metadataBaseUrl);
 
-    return {
-        title: post?.block?.[0]?.heading,
-        description: post?.block?.[0]?.subheading || (await parent).description,
-        openGraph: ogImageUrl
-            ? {
-                  images: [
-                      {
-                          url: ogImageUrl,
-                          alt: ogImageAlt,
-                      },
-                      ...((await parent).openGraph?.images || []),
-                  ],
-              }
-            : {},
-        metadataBase,
-    };
+	return {
+		title: post?.block?.[0]?.heading,
+		description: post?.block?.[0]?.subheading || (await parent).description,
+		openGraph: ogImageUrl
+			? {
+					images: [
+						{
+							url: ogImageUrl,
+							alt: ogImageAlt,
+						},
+						...((await parent).openGraph?.images || []),
+					],
+			  }
+			: {},
+		metadataBase,
+	};
 }
 
 export default async function PageSlugRoute({ params }) {
-    const currentPost = await sanityFetch<PostsPayload>({
-        query: postsBySlugQuery,
-        tags: ["post"],
-        qParams: { slug: params.slug },
-    });
+	const currentPost = await sanityFetch<PostsPayload>({
+		query: postsBySlugQuery,
+		tags: ["post"],
+		qParams: { slug: params.slug },
+	});
 
-    console.log(currentPost);
-    let otherPosts;
+	console.log(currentPost);
+	let otherPosts;
 
-    if (currentPost) {
-        const allPosts = await sanityFetch<PostsPayload[]>({
-            query: postsQuery,
-            tags: ["post"],
-        });
+	if (currentPost) {
+		const allPosts = await sanityFetch<PostsPayload[]>({
+			query: postsQuery,
+			tags: ["post"],
+		});
 
-        otherPosts = allPosts.filter(
-            (post) => post.slug?.current !== params.slug,
-        );
-    }
+		otherPosts = allPosts.filter((post) => post.slug?.current !== params.slug);
+	}
 
-    const blocks = currentPost?.block || [];
+	const blocks = currentPost?.block || [];
 
-    return (
-        <>
-            {currentPost && (
-                <>
-                    <main>
-                        {blocks?.map((block) => (
-                            <Blocks block={block as BlockProps} />
-                        ))}
-                    </main>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        {otherPosts && (
-                            <div className="flex-cols flex h-auto w-full flex-col bg-black p-2  pb-6 lg:px-6">
-                                <h4
-                                    className={`${monomaniac.className} text-4xl  text-gray-200 `}
-                                >
-                                    More Posts
-                                </h4>
-                                <div className="w-full py-12 ">
-                                    <PostsList post={otherPosts} />
-                                </div>
-                            </div>
-                        )}
-                    </Suspense>
-                </>
-            )}
-        </>
-    );
+	return (
+		<>
+			{currentPost && (
+				<>
+					<main>
+						{blocks?.map((block) => (
+							<Blocks block={block as BlockProps} />
+						))}
+					</main>
+					<Suspense fallback={<div>Loading...</div>}>
+						{otherPosts && (
+							<div className="flex-cols flex h-auto w-full flex-col bg-black p-2  pb-6 lg:px-6">
+								<h4
+									className={`${monomaniac.className} text-4xl  text-gray-200 `}
+								>
+									More Posts
+								</h4>
+								<div className="w-full py-12 ">
+									<PostsList post={otherPosts} />
+								</div>
+							</div>
+						)}
+					</Suspense>
+				</>
+			)}
+		</>
+	);
 }
