049e8a2419fd5786f7af02228c282096bfda5873
Raymond <raymond.luke.spartz@gmail.com>
2024-11-10 09:47:49 -0600

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,36 @@
 /** @type {import('next').NextConfig} */
 const config = {
-	images: {
-		remotePatterns: [
-			{ hostname: "cdn.sanity.io" },
-			{ hostname: "source.unsplash.com" },
-		],
-	},
-	typescript: {
-		// Set this to false if you want production builds to abort if there's type errors
-		ignoreBuildErrors: process.env.VERCEL_ENV === "production",
-	},
-	eslint: {
-		// Set this to false if you want production builds to abort if there's lint errors
-		ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
-	},
-	logging: {
-		fetches: {
-			fullUrl: true,
-		},
-	},
-	experimental: {
-		taint: true,
-		urlImports: ["https://themer.sanity.build/"],
-	},
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.sanity.io",
+        protocol: "https",
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/**`,
+      },
+      {
+        hostname: "source.unsplash.com",
+        protocol: "https",
+        pathname: "**",
+      },
+    ],
+  },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === "production",
+  },
+  eslint: {
+    // Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    taint: true,
+    urlImports: ["https://themer.sanity.build/"],
+  },
 };
 
 export default config;
