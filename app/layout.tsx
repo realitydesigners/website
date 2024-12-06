e7aba98d2f0044811f81e488205c32626a7667cc
Raymond <raymond.luke.spartz@gmail.com>
2024-12-06 12:26:47 -0600

    tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
 import { space } from "@/fonts";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import "tailwindcss/tailwind.css";
+import "./global.css";
 
 export const metadata: Metadata = {
-	title: "Reality Designers",
-	description:
-		"Reality Designers is a collective of artists, designers, and engineers who are building the next generation of immersive experiences.",
+  title: "Reality Designers",
+  description:
+    "Reality Designers is a collective of artists, designers, and engineers who are building the next generation of immersive experiences.",
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" className={`${space.className} bg-black`}>
-			<head>
-				<link rel="icon" href="/favicon.ico" sizes="any" />
-			</head>
-			<body className="bg-black">
-				{children}
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`${space.className} bg-black`}>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+      <body className="bg-black">
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
