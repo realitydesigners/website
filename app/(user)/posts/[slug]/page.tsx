444d5107407c97f3acdaf4c301a2adddcd648161
Raymond <raymond.luke.spartz@gmail.com>
2024-12-29 21:40:11 -0600

    .

--- app/(user)/posts/[slug]/page.tsx
+++ app/(user)/posts/[slug]/page.tsx
@@ -6,78 +6,68 @@ import { sanityFetch } from "@/sanity/lib/client";
 import { generateStaticSlugs } from "@/sanity/lib/generateStaticSlugs";
 import { PostsPayload } from "@/types";
 import { Metadata, ResolvingMetadata } from "next";
-import React, { Suspense } from "react";
+import React, { Suspense, useMemo } from "react";
 import { generatePageMetadata } from "@/lib/metadata";
-import { draftMode } from "next/headers";
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
+type Props = {
+  params: { slug: string };
+};
 
 export function generateStaticParams() {
   return generateStaticSlugs("posts");
 }
 
 export async function generateMetadata(
-  { params }: PageProps,
+  { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const post = await sanityFetch<PostsPayload>({
-    query: postsBySlugQuery,
-    qParams: { slug: params.slug },
-    tags: ["post"],
-  });
-
   return generatePageMetadata<PostsPayload>(
     {
       query: postsBySlugQuery,
-      slug: post?.slug?.current || "",
+      slug: params.slug,
       tags: ["post"],
     },
     parent
   );
 }
 
-export default async function PageSlugRoute({ params }: PageProps) {
-  const [currentPost, allPosts] = await Promise.all([
-    sanityFetch<PostsPayload>({
-      query: postsBySlugQuery,
-      qParams: { slug: params.slug },
-      tags: ["post"],
-    }),
-    sanityFetch<PostsPayload[]>({
+export default async function PageSlugRoute({ params }: Props) {
+  const currentPost = await sanityFetch<PostsPayload>({
+    query: postsBySlugQuery,
+    qParams: { slug: params.slug },
+    tags: [`category:${params.slug}`],
+  });
+
+  console.log(currentPost);
+  let otherPosts;
+
+  if (currentPost) {
+    const allPosts = await sanityFetch<PostsPayload[]>({
       query: postsQuery,
       tags: ["post"],
-    }),
-  ]);
+    });
 
-  if (!currentPost) {
-    return null;
+    otherPosts = allPosts.filter((post) => post.slug?.current !== params.slug);
   }
-
-  const otherPosts = allPosts.filter(
-    (post) => post.slug?.current !== currentPost.slug?.current
-  );
 
   const blocks = currentPost?.block || [];
 
   return (
     <>
-      <main>
-        {blocks?.map((block) => (
-          <Blocks key={block._key} block={block as BlockProps} />
-        ))}
-      </main>
-      <Suspense fallback={<div>Loading...</div>}>
-        {otherPosts && (
-          <div className="w-full py-16 px-4 lg:px-8">
-            <PostsList post={otherPosts} />
-          </div>
-        )}
-      </Suspense>
+      {currentPost && (
+        <>
+          <main>
+            {blocks?.map((block) => <Blocks block={block as BlockProps} />)}
+          </main>
+          <Suspense fallback={<div>Loading...</div>}>
+            {otherPosts && (
+              <div className="w-full py-16 px-4 lg:px-8">
+                <PostsList post={otherPosts} />
+              </div>
+            )}
+          </Suspense>
+        </>
+      )}
     </>
   );
 }
